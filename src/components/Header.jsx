0892ece830486bc112e35f18c159fabbb6996391
bethel ike <betheluzonduike@gmail.com>
2025-11-24 00:48:54 +0100

    added the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,43 @@
-export default function Header() {
-  return (
-    <section>
-      <p className="bg-blue-100">Hello from the Header</p>
-    </section>
-  );
+import React from "react"
+import logo from '../assets/poke-logo.png';
+export default function Header(){
+  const listBar = ["Home", "About","favorite"];
+  
+  return(
+    <nav className="bg-red-500 py-3 flex items-center space-x-10 px-6">
+       <ul className="flex justify-center items-center gap-10 font-bold text-1xl ml-10 ">
+        {listBar.map((el) => (<li>{el}</li>))}
+       </ul>
+
+        <img src={logo} alt="" className=" h-10 w-auto ml-auto"/>
+      </nav>
+
+  )
 }
+
+
+
+
+
+
+{/*
+ { const navClick =(onClick)=>{
+    console.log(onClick);
+
+  };
+  return (
+    <nav onClick= {(navClick)}className="bg-red-500 font-bold py-2.5 border border-gray-200 p-1 rounded">
+      <ul className=" flex justify-end gap-5 flex-wrap">
+        <li>Home</li>
+        <li>About us</li>
+        <li> favorite</li>
+      </ul>
+      <p></p>
+
+      <nav>
+
+      </nav>
+    </nav>
+  );
+ }
+*/}
