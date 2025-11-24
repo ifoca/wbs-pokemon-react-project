9e08b8004edd710a1e3b63a6f0dc38d3c09883dd
bethel ike <betheluzonduike@gmail.com>
2025-11-24 13:03:39 +0100

    made some chnages on the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,27 @@
 import React from "react"
 import logo from '../assets/poke-logo.png';
+
 export default function Header(){
   const listBar = ["Home", "About","favorite"];
   
   return(
-    <nav className="bg-red-500 py-3 flex items-center space-x-10 px-6">
-       <ul className="flex justify-center items-center gap-10 font-bold text-1xl ml-10 ">
-        {listBar.map((el) => (<li>{el}</li>))}
-       </ul>
+    <nav className="bg-red-500  flex items-center space-x-15 px-6 py-4">
+     <div className="flex justify-between items-center w-full">
+      <div className=""> 
+        <img src={logo} alt="" className=" h-10 w-auto ml-auto flex "/>
+      </div>
+      
+       <ul className=" flex justify-center items-end gap-20 font-bold text-1xl ml-10">
 
-        <img src={logo} alt="" className=" h-10 w-auto ml-auto"/>
+        {listBar.map((el) => (<li key={el} onClick={()=> console.loh(el)} className="cursor-pointer gap-3">{el}</li>))}
+      </ul>
+      </div>
       </nav>
 
   )
 }
 
-
+ 
 
 
 
