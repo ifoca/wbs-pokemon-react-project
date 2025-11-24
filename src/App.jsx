64b242658f579c1da73b6ce590c92ddcc3fd0222
ifoca <foca.ionut@gmail.com>
2025-11-24 12:05:58 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Header from './components/Header';
 import SearchBar from './components/SearchBar';
 import CardsList from './components/CardsList';
-import { useState } from 'react';
 import FilterFavorites from './components/FilterFavorites';
+import { useState } from 'react';
 
 function App() {
   let cards = [
