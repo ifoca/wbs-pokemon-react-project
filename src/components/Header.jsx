e9cd0f58e38775a69cd98f227e27e1a825ac2a2f
bethel ike <betheluzonduike@gmail.com>
2025-11-25 01:02:29 +0100

    change the event

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import logo from '../assets/poke-logo.png';
 
-
-  
 function Header (){
   const listBar = ["Home", "About","favorite"];
+  const handleClick=() =>{ 
+    console.log("Click");
+
+  };
   
   return(
     <nav className="bg-red-500  flex items-center space-x-15 px-6 py-4">
@@ -12,16 +14,13 @@ function Header (){
       <div className=""> 
         <img src={logo} alt="" className=" h-10 w-auto ml-auto flex "/>
       </div>
-      
        <ul className=" flex justify-center items-end gap-20 font-bold text-1xl ml-10">
-
-        {listBar.map((el) => (<li key={el} onClick={()=> console.log(el)} className="cursor-pointer gap-3">{el}</li>))}
+        {listBar.map((el) => (<li key={el} onClick={handleClick} className="cursor-pointer gap-3">{el}</li>))}
       </ul>
       </div>
       </nav>
 
   )
-
 }
 export default Header;
 
