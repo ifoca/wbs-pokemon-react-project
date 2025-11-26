d0dcbb24ae5ec79d615ec336bd6e22df1ffc7403
ifoca <foca.ionut@gmail.com>
2025-11-26 15:02:41 +0100

    small style change

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -3,7 +3,7 @@ import CardItem from './CardItem';
 export default function CardsList({ cards, onFavorite }) {
   return (
     <section className="cards-container bg-green-100">
-      <p>Hello from the Cards section</p>
+      <p className="text-center p-3">The world of Pokemon</p>
       <div className="cards-list grid grid-cols-4 gap-8 m-20">
         {cards.map((card) => (
           <CardItem key={card.id} card={card} onFavorite={onFavorite} />
